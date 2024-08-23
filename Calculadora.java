18c2dff17e2946292fa170dd5b6fdeee03f838b2
Airton-Santos <139139788+Airton-Santos@users.noreply.github.com>
2024-08-23 17:38:10 -0300

    Calculadora     2.0
    Adicionado a calculadora 1.0 e a 2.0

--- Calculadora.java
+++ Calculadora.java
@@ -1,15 +1,45 @@
-import java.util.Scanner; // Importa a classe Scanner para ler a entrada do usuario
+import java.util.Scanner; // Importa a classe Scanner para ler a entrada do usuário
 
 public class Calculadora {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in); //Criar um objeto para ler a entrada do usuario
+        Scanner scanner = new Scanner(System.in); // Cria um objeto Scanner para ler a entrada do usuário
 
-        System.out.println("Bem vindo a calculadora");
+        System.out.println("Bem-vindo à Calculadora!");
 
-        System.out.println("Digite o primeiro número");
-        double num1 = scanner.nextDouble();
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble(); // Lê o primeiro número digitado pelo usuário
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble(); // Lê o segundo número digitado pelo usuário
+
+        double soma = somar(num1, num2); // Chama o método somar e armazena o resultado na variável soma
+        double subtracao = subtrair(num1, num2); // Chama o método subtrair e armazena o resultado na variável subtracao
+        double multiplicacao = multiplicacao(num1, num2); // Chama o método multiplicacao e armazena o resultado na variável multiplicacao
+        double divisao = dividir(num1, num2); // Chama o método dividir e armazena o resultado na variável divisao
+
+        System.out.println("O Resultado da Soma é: " + soma);
+        System.out.println("O Resultado da Subtração é: " + subtracao);
+        System.out.println("O Resultado da Multiplicação é: " + multiplicacao);
+        System.out.println("O Resultado da Divisão é: " + divisao);
+
+        scanner.close();
     }
-    
+
+    public static double somar(double a, double b) {
+        return a + b; // Retorna a soma de dois números
+    }
+
+    public static double subtrair(double a, double b) {
+        return a - b; // Retorna a subtração de dois números
+    }
+
+    public static double multiplicacao(double a, double b) {
+        return a * b; // Retorna a multiplicação de dois números
+    }
+
+    public static double dividir(double a, double b) {
+        return a / b; // Retorna a divisão de dois números
+    }
 }
---
+++ CalculadoraTwo.java
@@ -0,0 +1,81 @@
+import java.util.Scanner; //Importa a classe Scanner para ler a entrada que o usuário colocar
+
+public class CalculadoraTwo {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in); // Cria um objeto Scanner para ler a entrada do usuário
+
+
+        System.out.println("Bem vindo a Calculadora 2.0!");
+
+        System.out.println("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Escolha a operação");
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+        int escolha = scanner.nextInt(); // Lê a escolha do usuário
+
+        double resultado = 0; // Variavél para armazenar o resultado da operação
+        
+        //usando Switch Case para diferentes tipos de escolhas
+
+        switch (escolha) {
+            case 1:
+                    resultado = somar(num1, num2); //Chama o método Somar Se a escolha for 1
+                    System.out.println("O resultado da Soma é: " + resultado);
+                break;
+
+            case 2:
+                    resultado = subtrair (num1, num2); //Chama o método Subtrair Se a escolha for 1
+                    System.out.println("O resultado da Subtração é: " + resultado);
+                break;
+
+            case 3:
+                resultado = multiplicacao(num1, num2); //Chama o método Multiplicar Se a escolha for 1
+                System.out.println("O resultado da Multiplicação é: " + resultado);
+            break;
+
+            case 4:
+                    if(num2 != 0) { //Verificar se o divisor não é 0 para não dar erro
+                        resultado = dividir(num1, num2); //Chama o método Dividir Se a escolha for 1
+                        System.out.println("O resultado da Divisão é: " + resultado);
+                    }
+                    else {
+                        System.out.println("Erro: Divisão por zero não é permitida!");
+                    }
+                break;
+
+            default:
+                    System.out.println("Escolha Inválida!");
+                    break;
+
+        }
+        
+        scanner.close(); //Fechar o scanner para não vazar dados!
+    }
+
+
+    public static double somar(double a, double b) {
+        return a + b; // Retorna a soma de dois números
+    }
+
+    public static double subtrair(double a, double b) {
+        return a - b; // Retorna a subtração de dois números
+    }
+
+    public static double multiplicacao(double a, double b) {
+        return a * b; // Retorna a multiplicação de dois números
+    }
+
+    public static double dividir(double a, double b) {
+        return a / b; // Retorna a divisão de dois números
+    }
+    
+}
